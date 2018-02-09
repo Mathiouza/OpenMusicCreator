7b5bf98ef3d9ac2a2c92043617b2441b35bbb852
Sourciluss <wtfangelx6@live.fr>
2018-02-09 11:44:19 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ var ejs = require('ejs');
 
 var app = express();
 
+//Gère le dossier img
+app.use(express.static(__dirname + '/img'));
+
 app.get('/editor', function(req, res) {
 	res.render('app.ejs', {});
 });
